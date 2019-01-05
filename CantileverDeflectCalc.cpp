074e098af2f3988d67a7d05f3334c5192d65c889
Alex Flamand <35501511+AlexFlamand@users.noreply.github.com>
2019-01-05 00:49:01 -0500

    Create CantileverDeflectCalc.cpp

---
+++ CantileverDeflectCalc.cpp
@@ -0,0 +1,47 @@
+// ALEX FLAMAND
+// 03/01/2018
+// #7
+
+// The purpose of this program is to determine the deflection of a beam of given dimensions supplied with thats end is affixed with a weight at half-foot increments. 
+// This is done by generating a for loop which calculates the deflection at each point until the sum of the increments equals the total length of the beam.
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double d, x, W, l, E, I, b, h;															// INITIALIZE VARIABLES
+
+	W = 200;																				// DEFINE VARIABLES
+	l = 3;
+	E = 187.2 * pow(10, 6);
+	b = 0.2;
+	h = 0.3;
+
+	I = (b * pow(h, 3)) / 12.0;																// CALCULATE AND DEFINE MOMENT OF INERTIA
+
+	cout << " CANTILEVER BEAM CALCULATOR" << endl;											// FORMATTING OF TITLE AND PARAMETERS
+	cout << "" << endl;
+	cout << "For a beam of 3.0 x 0.2 x 0.3 ft. "
+		 << "with a weight of 200 " << endl;
+	cout << "lbs. and an elastic modulus of 1.872 x 10^8 lbs/sqr(ft)." << endl; 
+	cout << "" << endl;
+	cout << setw(7) << "  Length: "
+		 << setw(11) << "      Deflection: " << endl;
+	cout << "-----------    -------------" << endl;
+	cout << setiosflags(ios::fixed) 
+		 << setiosflags(ios::showpoint) 
+		 << setprecision(5);
+
+	for (x = 0.0; x <= l; x = x + 0.5)														// FOR LOOP USED FOR MAIN CALCULATION
+	{
+		d = ((W * pow(x, 2)) / (6 * I * E)) * (3 * l - x);
+		cout << setw(7) << x << " ft.";
+		cout << setw(11) << d << " ft." << endl; 
+		cout << "" << endl;
+	}
+
+	return 0;
+}
